93383a0c265d4340bc1dcf5b8513d9d55a87b0c7
Egor-shik <vidosyzelenka@yandex.ru>
2025-07-29 18:39:22 +0300

    6th commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,24 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
 	"os"
-	"os/signal"
+	"os/exec"
+
 	"sprint-6-final/internal/server"
-	"syscall"
-	"time"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "MORSE CONVERTER: ", log.LstdFlags|log.Lshortfile)
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	srv := server.New(logger, "8080")
+	server := server.NewServer(logger)
 
-	// Канал для graceful shutdown
-	done := make(chan bool, 1)
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 
-	go func() {
-		logger.Println("Starting server on :8080")
-		if err := srv.Start(); err != nil && err != http.ErrServerClosed {
-			logger.Fatalf("Could not start server: %v", err)
-		}
-	}()
-
-	// Ждем сигнала завершения
-	<-quit
-	logger.Println("Shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		logger.Fatalf("Could not gracefully shutdown: %v", err)
+	log.Println("Сервер запускается...")
+	if err := server.Start(); err != nil {
+		logger.Fatalf("Ошибка запуска сервера: %v", err)
 	}
-
-	close(done)
-	<-done
-	logger.Println("Server stopped")
 }
